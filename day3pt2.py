b0ba55da9e3cf18075bed339d3b011f678021b7d
elisecode247 <elisecode247@gmail.com>
2024-12-02 23:02:22 -0800

    day 3 fixed

--- day3pt2.py
+++ day3pt2.py
@@ -2,20 +2,19 @@ import sys
 import re
 
 sum = 0
-text = ""
-with open('day3 copy.txt', 'r') as file:
-    for line in file.read().splitlines():
-        text += line
-strings = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)", text)
 start = True
-for string in strings:
-    if string == "do()":
-        start = True
-    elif string == "don't()":
-        start = False
-    elif start == True:
-        x = string.split('(')[1].split(')')[0].split(',')
-        x = int(x[0]) * int(x[1])
-        sum += x
-print(sum) # This time, the sum of the results is 48 (2*4 + 8*5).
+with open('day3.txt', 'r') as file:
+    for line in file.read().splitlines():
+        strings = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)", line)
+
+        for string in strings:
+            if string == "do()":
+                start = True
+            elif string == "don't()":
+                start = False
+            elif start == True:
+                x = string.split('(')[1].split(')')[0].split(',')
+                x = int(x[0]) * int(x[1])
+                sum += x
+print(sum)
 
